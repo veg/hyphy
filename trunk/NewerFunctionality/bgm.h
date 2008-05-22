51abfc485fd6f06db6d412840a40272df9c5844a
Art Poon <artpoon@gmail.com>
2008-05-22 23:44:47 +0000

    - add lots of functions for graph MCMC, impute missing data, member variable access - new member variables for stuff like learning network parameters, setting node orders, restarting chains... - take dynamic BGMs out of the picture for now
    git-svn-id: http://www.datam0nk3y.org/svn/hyphy@54 8514abd6-fbe8-4789-b558-0fd185ebde39

--- trunk/NewerFunctionality/bgm.h
+++ trunk/NewerFunctionality/bgm.h
@@ -20,7 +20,7 @@
 class Bgm : public _LikelihoodFunction
 {
 public:
-	Bgm (_AssociativeList *, _AssociativeList *, _AssociativeList *);		// constructor
+	Bgm (_AssociativeList *, _AssociativeList *);		// constructor
 	
 	virtual ~Bgm (void);		// destructor
 	
@@ -34,34 +34,46 @@ public:
 				
 	
 	virtual _Matrix *		Optimize ();	// estimate maximum posterior network via greedy hill-climbing heuristic
+	
+	//virtual _Matrix *		Optimize (_SimpleList *);
 					
 	virtual _PMathObj		CovarianceMatrix (_SimpleList *);	// wrapper function for MCMC, to explore posterior distribution 
 	
 	
 	
 	void			SetDataMatrix (_Matrix *),
-					SetWeightMatrix (_Matrix *);
-	
+					SetWeightMatrix (_Matrix *),
+					SetGraphMatrix (_Matrix *),
+					SetBanMatrix (_Matrix *),
+					SetEnforceMatrix (_Matrix *),
+					SetBestOrder (_SimpleList *);
 	
 	_Matrix *		ExportNodeScores (void);
+	_Matrix *		ExportGraph (void);
 	
 	void			ImportNodeScores (_Matrix *);
 	
 	
 	_Parameter		ComputeDiscreteScore (long node_id),	// compute K2 or BDeu scoring metric for a discrete node in network
-					// with discrete-valued parents, given data and prior
-					// use BDeu if prior_sample_size > 0.
+															// with discrete-valued parents, given data and prior
+															// use BDeu if prior_sample_size > 0.
+					ComputeDiscreteScore (long, _Matrix *),
 					ComputeDiscreteScore (long, _SimpleList &),
 					
+					ImputeDiscreteScore (long, _SimpleList &),
 					
-					ComputeContinuousScore (long node_id);	// compute scoring metric for continuous node with both discrete
-					// and continuous parents.
+					ComputeContinuousScore (long node_id),	// compute scoring metric for continuous node with both discrete
+															// and continuous parents.
+					ComputeContinuousScore (long, _Matrix *);
 	
+	long			GetNumNodes (void)		{ return num_nodes; }
+	long			GetNumCases (void)		{ return (obsData ? obsData->GetHDim() : 0); }
+	_SimpleList		GetLastOrder (void)		{ return last_node_order; }
 	
 protected:
-	_Parameter		Compute (_SimpleList*, _List*);			// return likelihood of node ordering, model averaging over all
+	_Parameter		Compute (_SimpleList*, _List*),			// return likelihood of node ordering, model averaging over all
 															// possible networks that are consistent with the order.
-	
+					Compute (_Matrix *);
 	
 	void			CacheNodeScores (void);
 	void			ReleaseNodeScores (void);
@@ -72,19 +84,21 @@ protected:
 	void			DumpComputeLists (_List *);
 	
 	
-	_Parameter		LnGamma (_Constant *, long),
+	_Parameter		LnGamma (_Constant *, _Parameter),
 					LogSumExpo (_GrowingVector *);
 	
 	_Matrix *		RunColdChain (_SimpleList *, long, long);
 	void			RunHotChain (_SimpleList *, long, long, _Parameter);
 	
+	_Matrix *		GraphMCMC (bool);
 	
 		/* ------------- these functions are used only for greedy search heuristic ---------------- */
 	_Parameter		TryEdge (long, long, long, _Parameter);
 	
 	void			RandomizeDag (long),
-					PrintDag (void),
-					ResetDag (void);
+					RandomizeGraph (_Matrix *, _SimpleList *, long, bool),
+					PrintGraph (_Matrix *),
+					ResetGraph (_Matrix *);
 	
 	long			MarginalSum (bool, long);
 	
@@ -103,8 +117,10 @@ private:
 	
 	_Matrix			dag,				// i.e. directed acyclic graph as a square adjacency matrix,
 										// where 1 indicates presence of edge (row --> col)
+					
 	
 					banned_edges,		// complements DAG, 1 entry indicates banned edge
+					enforced_edges,
 	
 					prior_sample_size,	// prior distributions for hyperparameters
 					prior_mean,
@@ -115,10 +131,15 @@ private:
 		
 					num_levels,			// vector containing number of levels for discrete nodes
 					
-					max_parents;		// maximum number of parents allowed per node
-	
+					max_parents,		// maximum number of parents allowed per node
 					
-	_List			node_scores;
+					has_missing,		// data matrix for i-th node contains missing values
+					
+					last_node_order,	// store node orderings from order-MCMC
+					best_node_order;
+					
+	_List			node_scores,
+					dag_parameters;
 	
 	
 	_Matrix		*	obsData,			// data matrix read from file
@@ -133,6 +154,7 @@ private:
 
 
 /* ____________________________________________________________________________________________________________________________________	*/
+#ifdef __NEVER_DEFINED__
 class _DynamicBgm : public Bgm
 {
 public:
@@ -145,7 +167,7 @@ private:
 
 
 bool		ReadDataFromFile	(char, _Matrix &);
-
+#endif
 
 
 
