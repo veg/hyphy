5cb4123d8d2ed3c7fc0f7f2bac244b815fbe70bc
Art Poon <artpoon@gmail.com>
2008-10-14 22:12:52 +0000

    - split bgm.cpp source into two files, bgm.cpp and bgm2.cpp, the latter reserved for methods in development - re-implemented missing data imputation in bgm2.cpp using candidate method (analogous to Gibbs sampling).
    git-svn-id: http://www.datam0nk3y.org/svn/hyphy@254 8514abd6-fbe8-4789-b558-0fd185ebde39

--- trunk/NewerFunctionality/bgm.h
+++ trunk/NewerFunctionality/bgm.h
@@ -17,15 +17,40 @@
 #include "batchlan.h"
 // #include "HYUtils.h"
 
+
+/*SLKP 20070926; include progress report updates */
+#if !defined __UNIX__ && !defined __HEADLESS__
+	#include "HYConsoleWindow.h"
+#endif
+/*SLKP*/
+
+
+#if defined __AFYP_DEVELOPMENT__ && defined __HYPHYMPI__
+	#include "mpi.h"
+#endif
+
+
+#define		LOG_SCALING_FACTOR			64.
+#define		LARGE_NEGATIVE_NUMBER		-99999.0
+#define		MAX_LSE_SCALING_ATTEMPTS	4096
+
+#define		MAX_FAIL_RANDOMIZE			1000
+#define		RANDOMIZE_PROB_SWAP			0.1
+
+#define		DIRICHLET_FLATTENING_CONST	0.5
+
+
+
+
 /* ____________________________________________________________________________________________________________________________________	*/
 class Bgm : public _LikelihoodFunction
 {
 public:
+	Bgm ()	{ /* default constructor does diddly squat */ }
+	
 	Bgm (_AssociativeList *, _AssociativeList *);		// constructor
 	
 	virtual ~Bgm (void);		// destructor
-	
-	
 					
 	virtual	_Parameter		Compute (void);			// function polymorphism, no argument returns likelihood of network
 													// specified by _Matrix object dag
@@ -42,79 +67,84 @@ public:
 	
 	
 	
-	void			SetDataMatrix (_Matrix *),
-					SetWeightMatrix (_Matrix *),
-					SetGraphMatrix (_Matrix *),
-					SetBanMatrix (_Matrix *),
-					SetEnforceMatrix (_Matrix *),
-					SetBestOrder (_SimpleList *);
+	virtual void			SetDataMatrix (_Matrix *);
+					
+			void			SetWeightMatrix (_Matrix *),
+							SetGraphMatrix (_Matrix *),
+							SetBanMatrix (_Matrix *),
+							SetEnforceMatrix (_Matrix *),
+							SetBestOrder (_SimpleList *);
 	
-	_Matrix *		ExportNodeScores (void);
-	_Matrix *		ExportGraph (void);
+			_Matrix *		ExportNodeScores (void);
+			_Matrix *		ExportGraph (void);
 	
-	void			SerializeBgm (_String &);
+			void			SerializeBgm (_String &);
 	
-	void			ImportNodeScores (_Matrix *);
+			void			ImportNodeScores (_Matrix *);
 	
 	
-	_Parameter		ComputeDiscreteScore (long node_id),	// compute K2 or BDeu scoring metric for a discrete node in network
+			_Parameter		ComputeDiscreteScore (long node_id),	// compute K2 or BDeu scoring metric for a discrete node in network
 															// with discrete-valued parents, given data and prior
 															// use BDeu if prior_sample_size > 0.
-					ComputeDiscreteScore (long, _Matrix *),
-					ComputeDiscreteScore (long, _SimpleList &),
-					
-					ImputeDiscreteScore (long, _SimpleList &),
-					
-					ComputeContinuousScore (long node_id),	// compute scoring metric for continuous node with both discrete
-															// and continuous parents.
-					ComputeContinuousScore (long, _Matrix *),
-					ComputeContinuousScore (long, _SimpleList &, _SimpleList &);
+							ComputeDiscreteScore (long, _Matrix *),
+							ComputeDiscreteScore (long, _SimpleList &),
+							
+							ComputeContinuousScore (long node_id),	// compute scoring metric for continuous node with both discrete
+																	// and continuous parents.
+							ComputeContinuousScore (long, _Matrix *),
+							ComputeContinuousScore (long, _SimpleList &, _SimpleList &);
 	
-	long			GetNumNodes (void)		{ return num_nodes; }
-	long			GetNumCases (void)		{ return (obsData ? obsData->GetHDim() : 0); }
-	_SimpleList		GetLastOrder (void)		{ return last_node_order; }
+	virtual _Parameter		ImputeDiscreteScore (long, _SimpleList &);
+			
+			_Parameter		GibbsApproximateDiscreteScore (long, _SimpleList &),
+							K2Score (long, _Matrix &, _Matrix &),
+							BDeScore (long, _Matrix &, _Matrix &);
+	
+			long			GetNumNodes (void)		{ return num_nodes; }
+			long			GetNumCases (void)		{ return (obsData ? obsData->GetHDim() : 0); }
+			_SimpleList		GetLastOrder (void)		{ return last_node_order; }
 	
 protected:
-	_Parameter		Compute (_SimpleList*, _List*),			// return likelihood of node ordering, model averaging over all
-															// possible networks that are consistent with the order.
-					Compute (_Matrix *);
+			_Parameter		Compute (_SimpleList*, _List*),			// return likelihood of node ordering, model averaging over all
+																	// possible networks that are consistent with the order.
+							Compute (_Matrix *),
+							ComputeDynamic (_SimpleList *, _List *),	// compute for DBNs
+							ComputeDynamic2 (_SimpleList *, _List *);
+					
 	
-	void			CacheNodeScores (void);
-	void			ReleaseNodeScores (void);
+	virtual void			CacheNodeScores (void);
+			void			ReleaseNodeScores (void);
+	
+			void			MPICacheNodeScores (long);
+			void			MPIReceiveScores (_Matrix *, bool, long);
+			void			CacheNetworkParameters (void);
+	
+	
+			void			InitComputeLists (_List *);
+			void			DumpComputeLists (_List *);
 
-	void			MPICacheNodeScores (long);
-	void			MPIReceiveScores (_Matrix *, bool, long);
-	void			CacheNetworkParameters (void);
+			
 	
-	//unsigned long	IndexIntoCache (long, _SimpleList);
-	//void			IndicesFromCache (long);
+			_Parameter		LnGamma (_Constant *, _Parameter),
+							LogSumExpo (_GrowingVector *);
+			
+			_Matrix *		RunColdChain (_SimpleList *, long, long);
+			void			RunHotChain (_SimpleList *, long, long, _Parameter);
+			
+			_Matrix *		GraphMCMC (bool);
 	
-	void			InitComputeLists (_List *);
-	void			DumpComputeLists (_List *);
+			_Parameter		TryEdge (long, long, long, _Parameter);		// DEPRECATE
+			
+			void			RandomizeDag (long),
+							RandomizeGraph (_Matrix *, _SimpleList *, long, bool),
+							PrintGraph (_Matrix *),
+							ResetGraph (_Matrix *);
+			
+			long			MarginalSum (bool, long);	// DEPRECATE
+			
+			bool			IsCyclic (void);	// DEPRECATE
 	
-	
-	_Parameter		LnGamma (_Constant *, _Parameter),
-					LogSumExpo (_GrowingVector *);
-	
-	_Matrix *		RunColdChain (_SimpleList *, long, long);
-	void			RunHotChain (_SimpleList *, long, long, _Parameter);
-	
-	_Matrix *		GraphMCMC (bool);
-	
-		/* ------------- these functions are used only for greedy search heuristic ---------------- */
-	_Parameter		TryEdge (long, long, long, _Parameter);
-	
-	void			RandomizeDag (long),
-					RandomizeGraph (_Matrix *, _SimpleList *, long, bool),
-					PrintGraph (_Matrix *),
-					ResetGraph (_Matrix *);
-	
-	long			MarginalSum (bool, long);
-	
-	bool			IsCyclic (void);
-		/* ----------------------------------------------------------------------------------------	*/
-	
-private:
+
 	
 	long			num_nodes,			// number of variables in network
 	
@@ -156,27 +186,31 @@ private:
 				*	obsWeights;			// vector of weights corresponding to each observation in data matrix
 		
 	
-	bool			scores_cached,
-					is_dynamic_graph;
+	bool			scores_cached;
 };
 
 
 
 /* ____________________________________________________________________________________________________________________________________	*/
-#ifdef __NEVER_DEFINED__
+
 class _DynamicBgm : public Bgm
 {
-public:
-	_DynamicBgm ();		// default constructor does nothing
-	_DynamicBgm (_AssociativeList *, _AssociativeList *, _AssociativeList *);
 	
-	virtual ~_DynamicBgm (void);		// destructor
+public:
+	_DynamicBgm (_AssociativeList *, _AssociativeList *);
+	
+	virtual ~_DynamicBgm (void)			{ } // destructor
+	
+	virtual	void		CacheNodeScores		(void);
+			void		CollapseNodeSpace	(void);
+	
+			void		CollapseDynamicGraph (void);
 private:
+
 };
 
 
-bool		ReadDataFromFile	(char, _Matrix &);
-#endif
+
 
 
 
