b7df6f72761268bea37e233f97e4b4008966266f
Sergei L Kosakovsky Pond <spond@ucsd.edu>
2010-09-14 18:26:01 +0000

    git-svn-id: http://www.datam0nk3y.org/svn/hyphy@963 8514abd6-fbe8-4789-b558-0fd185ebde39

--- trunk/Core/strings.cpp
+++ trunk/Core/strings.cpp
@@ -39,7 +39,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <time.h>
 
 
-_String	  __KERNEL__VERSION__ ("2.0020100903beta");
+_String	  __KERNEL__VERSION__ ("2.0020100914beta");
 
 #ifdef	 __UNIX__
 	#if !defined __MINGW32__
