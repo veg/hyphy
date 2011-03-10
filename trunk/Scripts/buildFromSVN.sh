5cbeafd19c00149bed0e4ba760b250d87a0e9926
Sergei L Kosakovsky Pond <spond@ucsd.edu>
2011-03-10 23:54:50 +0000

    git-svn-id: http://www.datam0nk3y.org/svn/hyphy@1073 8514abd6-fbe8-4789-b558-0fd185ebde39

--- trunk/Scripts/buildFromSVN.sh
+++ trunk/Scripts/buildFromSVN.sh
@@ -37,6 +37,7 @@ cp trunk/HeadlessLink/*.{h,cpp} $installDirectory/Source/Link
 
 cp trunk/NewerFunctionality/*.{h,cpp} $installDirectory/Source/
 cp -R trunk/Library $installDirectory/
+cp -R trunk/data $installDirectory/
 cp SQLite/trunk/*.{c,h} $installDirectory/Source/SQLite/
 cp trunk/Scripts/*.sh $installDirectory/
 cp 'trunk/GUIElements/Shared Source/'*.cpp $installDirectory/GUI/
