d87676c6532d7d06190ff8896a91d64675e2056f
Steven Weaver <sweaver@ucsd.edu>
2011-08-04 21:15:29 +0000

    Removing binaries from svn
    git-svn-id: http://www.datam0nk3y.org/svn/hyphy@1213 8514abd6-fbe8-4789-b558-0fd185ebde39

--- trunk/UnitTests/gtest-1.6.0/xcode/build/gtest.build/Release/gtest-framework.build/DerivedSources/gtest_vers.c
+++
@@ -1,1 +0,0 @@
- const unsigned char gtestVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:gtest  PROJECT:gtest-" "\n"; const double gtestVersionNumber __attribute__ ((used)) = (double)0.;
--- trunk/UnitTests/gtest-1.6.0/xcode/build/gtest.build/Release/gtest.build/Script-40C44ADB0E3798F4008FCC51.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-# Remember, this "Run Script" build phase will be executed from $SRCROOT
-/usr/bin/python Scripts/versiongenerate.py ../ $PROJECT_TEMP_DIR
