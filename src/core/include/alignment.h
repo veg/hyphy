28e5aa8c3ce74b913c7013517eb13b33b73f90ed
Lance Hepler <nlhepler@gmail.com>
2012-02-20 14:40:36 -0800

    Move alignment stuff out of batchlan2 and into its own file

---
+++ src/core/include/alignment.h
@@ -0,0 +1,47 @@
+
+#include "baseobj.h"
+#include "likefunc.h"
+#include "matrix.h"
+#include "simplelist.h"
+
+_Parameter AlignStrings( _String * r_str
+                       , _String * q_str
+                       , _SimpleList & char_map
+                       , _Matrix * cost_matrix
+                       , const char gap
+                       , _Parameter open_insertion
+                       , _Parameter extend_insertion
+                       , _Parameter open_deletion
+                       , _Parameter extend_deletion
+                       , _Parameter miscall_cost
+                       , const bool do_local
+                       , const bool do_affine
+                       , const bool do_codon
+                       , _List & store
+                       , const long char_count
+                       , _Matrix * codon3x5
+                       , _Matrix * codon3x4
+                       , _Matrix * codon3x2
+                       , _Matrix * codon3x1
+                       );
+
+_Parameter LinearSpaceAlign( _String * s1           // first string
+                           , _String * s2           // second string
+                           , _SimpleList & cmap     // char -> position in scoring matrix mapper
+                           , _Matrix * ccost        // NxN matrix of edit distances on characters
+                           , _Parameter gopen       // the cost of opening a gap in sequence 1
+                           , _Parameter gextend     // the cost of extending a gap in sequence 1 (ignored unless doAffine == true)
+                           , _Parameter gopen2      // the cost of opening a gap in sequence 2
+                           , _Parameter gextend2    // the cost of opening a gap in sequence 2   (ignored unless doAffine == true)
+                           , bool doLocal           // ignore prefix and suffix gaps
+                           , bool doAffine          // use affine gap penalties
+                           , _SimpleList & ops      // edit operations for the optimal alignment
+                           , _Parameter scoreCheck  // check the score of the alignment
+                           , long from1
+                           , long to1
+                           , long from2
+                           , long to2
+                           , _Matrix ** buffer      // matrix storage,
+                           , char parentGapLink
+                           , char * ha
+                           );
