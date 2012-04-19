63e118d1da8bd4520d98ce14cef54e8c51827625
Lance Hepler <nlhepler@gmail.com>
2012-04-19 14:54:20 -0700

    tag 2.1.2: fix sqlite3 for python lib

--- src/lib/setup.py
+++ src/lib/setup.py
@@ -21,7 +21,7 @@ hyphyPath, srcDir = path.split(srcPath)
 #     resFiles = [(f, path.join(*(['..'] * 5 + f.split('/')))) for f in fh.read().split('\n') if f != '']
 
 contribPath = path.join(hyphyPath, 'contrib')
-sqlitePath = path.join(contribPath, 'SQLite-3.6.17')
+sqlitePath = path.join(contribPath, 'SQLite-3.7.11')
 
 linkPath = path.join(scriptPath, 'Link')
 coreSrcPath = path.join(srcPath, 'core')
