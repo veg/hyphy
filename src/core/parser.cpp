veg/hyphy

commit 0e1afebf40a74864da7e9ee5815f40714e77af99

Made UnOps into a Trie

src/core/parser.cpp
@@ -81,8 +81,9 @@ _SimpleList     FunctionArgumentCount,
 bool            useGlobalUpdateFlag = false;
 
 
-_String     UnOps ("-,!,+,Abs,Sin,Cos,Tan,Exp,Log,Arctan,Time,Gamma,Transpose,Sqrt,Erf,Rows,Columns,LUDecompose,Inverse,BranchCount,TipCount,ZCDF,Eigensystem,Simplex,Type,Eval,LnGamma,"),
-            HalfOps (":<>=!&|");
+_String         HalfOps (":<>=!&|");
+
+_Trie           UnOps;
 
 _SimpleList opPrecedence,
             BinOps,
@@ -697,7 +698,38 @@ void  ReplaceVar (_Variable* theV)
 void    SetupOperationLists (void)
 {
 
+    _List all_unary_ops ("-",26,
+                          "!",
+                         "+",
+                         "Abs",
+                         "Sin",
+                         "Cos",
+                         "Tan",
+                         "Exp",
+                         "Log",
+                         "Arctan",
+                         "Time",
+                         "Gamma",
+                         "Transpose",
+                         "Sqrt",
+                         "Erf",
+                         "Rows",
+                         "Columns",
+                         "LUDecompose",
+                         "Inverse",
+                         "BranchCount",
+                         "TipCount",
+                         "ZCDF",
+                         "Eigensystem",
+                         "Simplex",
+                         "Type",
+                         "Eval",
+                         "LnGamma"
+                         );
+ 
 
+    UnOps.Insert (all_unary_ops);
+    
     BinOps<<'|'*256+'|';
     opPrecedence<<1;
     BinOps<<'&'*256+'&';

src/core/trie.cpp
@@ -195,6 +195,17 @@ long     _Trie::Find (const _String& key, _SimpleList* path, bool prefixOK){
 }
 
 //----------------------------------------------------------------------------------------------------------------------
+long     _Trie::Find (const char key, bool prefixOK){
+    long current_index = 0,
+    next_index    = FindNextLetter (key, current_index);
+    if (next_index < 0 && prefixOK) {
+        next_index = FindNextLetter (0, current_index);
+        current_index = next_index;
+    }
+    return current_index;
+}
+
+//----------------------------------------------------------------------------------------------------------------------
 long     _Trie::GetValueFromString (const _String& key){
     long keyIndex = Find(key);
     if (keyIndex != HY_TRIE_NOTFOUND) {

src/core/include/likefunc.h
@@ -291,7 +291,7 @@ public:
     }
 
     /**
-       @return  an associative array with a variety of metrics (see http://www.datam0nk3y.org/hyphywiki/index.php/GetString#Functionality)
+       @return  an associative array with a variety of metrics (see GetString on HyPhy wiki)
                 about this LF.
        @author  SLKP
        @version 20110608

src/core/include/parser.h
@@ -58,6 +58,8 @@ SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #include "stack.h"
 #include "variable.h"
 #include "variablecontainer.h"
+#include "trie.h"
+
 //End parser specific includes
 
 #include "matrix.h"
@@ -79,8 +81,9 @@ extern      _SimpleList     BuiltInFunctionParameterCount,
 
 extern      _AVLListX       variableNames;
 
-extern      _String         UnOps,
-            HalfOps;
+extern      _String         HalfOps;
+
+extern      _Trie           UnOps;
 
 extern      _Parameter      printDigits,
             verbosityLevel;

src/core/include/trie.h
@@ -134,7 +134,15 @@ class _Trie: public _List
          * @param  prefixOK -- returns a match if a prefix of 'key' in the trie
          * @return the index of the key in 'nodes' if found, HY_TRIE_NOTFOUND/HY_TRIE_INVALID_LETTER otherwise  
          */
-        
+
+        long     Find (const char key, bool prefixOK = false);
+        /**
+         * Determine if 'key' is in the trie
+         * @param  key      -- the character to search for
+         * @param  prefixOK -- returns a match if a prefix of 'key' in the trie
+         * @return the index of the key in 'nodes' if found, HY_TRIE_NOTFOUND/HY_TRIE_INVALID_LETTER otherwise
+         */
+
         long     GetValueFromString (const _String& key);
         /**
          * A convenience function which calls Find and then GetValue if teh key is found

src/core/likefunc.cpp
@@ -3856,6 +3856,19 @@ _Matrix*        _LikelihoodFunction::Optimize ()
 
     SetupLFCaches       ();
     SetupCategoryCaches ();
+    
+    
+    /*for (long k = 0; k < theTrees.lLength; k++){
+        _SimpleList subset(k);
+        _String          sLF (8192L, true);
+        SerializeLF      (sLF,_hyphyLFSerializeModeVanilla,&subset,nil);
+        sLF.Finalize     ();
+        FILE * F = doFileOpen(_String("/tmp/") & k & ".dump", "w");
+        printf ("%ld\n",k);
+        fprintf (F, "%s", sLF.sData);
+        fclose (F);
+    }*/
+
 
 #ifdef __HYPHYMPI__
     if (_hy_mpi_node_rank == 0) {
@@ -10594,7 +10607,7 @@ void    _LikelihoodFunction::PrepareToCompute (bool disableClear)
             hasBeenSetUp ++;
         }
         siteArrayPopulated = false;
-
+ 
     } else {
         hasBeenSetUp++;
     }

src/core/parser2.cpp
@@ -1117,7 +1117,7 @@ long        Parse (_Formula* f, _String& s, long& variableReference, _VariableCo
                 globalKey  = true;
             }
                 
-            if (UnOps.contains (_String(',')&curOp&',')) { // a standard function
+            if (UnOps.Find(curOp)>=0) { // a standard function
                 levelOps->AppendNewInstance (new _Operation (curOp,1));
                 continue;
             } else { // a variable
@@ -1241,7 +1241,7 @@ long        Parse (_Formula* f, _String& s, long& variableReference, _VariableCo
 
             if (levelData->countitems()==0) {
                 if (s[i-curOp.sLength]!=')' && storage!=')' && s[i-curOp.sLength] !=']') {
-                    if (!twoToken && UnOps.contains(s.getChar(i))) {
+                    if (!twoToken && UnOps.Find (s.getChar(i)) >= 0) {
                         twoOrOne = 1;
                     } else {
                         return HandleFormulaParsingError ("Bad binary operator placement ", saveError, s, i);
@@ -1323,7 +1323,7 @@ long        Parse (_Formula* f, _String& s, long& variableReference, _VariableCo
                 return HY_FORMULA_FAILED;
             }
             continue;
-        } else if (UnOps.contains(s.getChar(i))) {
+        } else if (UnOps.Find(s.getChar(i)) >= 0) {
             if ((s.getChar(i)=='-' || s.getChar(i)=='+') && (!i|| s.getChar(i-1)=='(')) { // unary minus?
                 curOp   = s.getChar(i);
                 levelOps->AppendNewInstance (new _Operation (curOp,1));

commit de496e22809657208fdf63e0a645b2d5644055eb

Fixed Issue #134 which was introduced by the commit 0e1afebf40a74864da7e9ee5815f40714e77af99, which in turned revealed a bug in the Trie class (incorrectly handled single letter strings)

src/core/parser.cpp
@@ -699,7 +699,7 @@ void    SetupOperationLists (void)
 {
 
     _List all_unary_ops ("-",26,
-                          "!",
+                         "!",
                          "+",
                          "Abs",
                          "Sin",

src/core/trie.cpp
@@ -200,8 +200,8 @@ long     _Trie::Find (const char key, bool prefixOK){
     next_index    = FindNextLetter (key, current_index);
     if (next_index < 0 && prefixOK) {
         next_index = FindNextLetter (0, current_index);
-        current_index = next_index;
     }
+    current_index = next_index;
     return current_index;
 }
 
@@ -257,7 +257,7 @@ long    _Trie::Insert (const _String& key, const long value) {
     if (next_index == HY_TRIE_INVALID_LETTER)
         return HY_TRIE_INVALID_LETTER;
     
-    if (current_char == key.sLength)
+    if (current_char == key.sLength && next_index >= 0)
         return next_index;
     
     current_char --;