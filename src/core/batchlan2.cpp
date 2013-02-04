veg/hyphy

commit 35db76dd51f35058b53f15956af3bfd2538d32dd

Fixes issue #140

src/core/batchlan2.cpp
@@ -2173,7 +2173,7 @@ void    RetrieveModelComponents (long mid, _Matrix*& mm, _Matrix*& fv, bool & mb
 
 void    RetrieveModelComponents (long mid, _Variable*& mm, _Variable*& fv, bool & mbf)
 {
-    if (modelTypeList.lData[mid] == 0) {
+    if (mid >= 0 && modelTypeList.lData[mid] == 0) {
         mm = LocateVar(modelMatrixIndices.lData[mid]);
     } else {
         mm = nil;
@@ -2198,20 +2198,26 @@ bool    IsModelReversible (long mid)
     return false;
 }
 
+//____________________________________________________________________________________
+
+bool    IsModelOfExplicitForm (long modelID) {
+    if (modelID != HY_NO_MODEL) {
+        return modelTypeList.lData[modelID] != 0;
+    }
+    return false;
+}
 
 //____________________________________________________________________________________
 
 void    ScanModelForVariables        (long modelID, _AVLList& theReceptacle, bool inclG, long modelID2, bool inclCat)
 {
     if (modelID != HY_NO_MODEL) {
-        if (modelTypeList.lData[modelID] == 0)
-            // standard rate matrix
-        {
+        // standard rate matrix
+        if (modelTypeList.lData[modelID] == 0) {
             ((_Matrix*) (LocateVar(modelMatrixIndices.lData[modelID])->GetValue()))->ScanForVariables2(theReceptacle,inclG,modelID2,inclCat);
-        } else
-            // formula based
-        {
-            ((_Formula*)modelMatrixIndices.lData[modelID])->ScanFForVariables(theReceptacle, inclG, false, inclCat);
+        } else {
+        // formula based
+            ((_Formula*)modelMatrixIndices.lData[modelID])->ScanFForVariables(theReceptacle, false, false, inclCat);
         }
     }
 }
@@ -2322,6 +2328,9 @@ BaseRef _HYRetrieveBLObjectByName    (_String& name, long& type, long *index, bo
             type = HY_BL_MODEL;
             if (index) {
                 *index = loc;
+            }
+            if (IsModelOfExplicitForm(loc)) {
+                return (BaseRef)modelMatrixIndices.lData[loc];
             }
             return LocateVar (modelMatrixIndices.lData[loc]);
         }

src/core/formula.cpp
@@ -1659,8 +1659,9 @@ void _Formula::ScanFForVariables (_AVLList&l, bool includeGlobals, bool includeA
         _Operation* theObj = ((_Operation**)theFormula.lData)[i];
         if (theObj->IsAVariable()) {
             if (!includeGlobals)
-                if ((((_Variable*)LocateVar(theObj->GetAVariable()))->IsGlobal())||
-                        (((_Variable*)LocateVar(theObj->GetAVariable()))->ObjectClass()!=NUMBER)) {
+                //if ((((_Variable*)LocateVar(theObj->GetAVariable()))->IsGlobal())||
+                 //       (((_Variable*)LocateVar(theObj->GetAVariable()))->ObjectClass()!=NUMBER)) {
+                if (((_Variable*)LocateVar(theObj->GetAVariable()))->IsGlobal()) {
                     continue;
                 }
 

src/core/batchlanruntime.cpp
@@ -1002,21 +1002,23 @@ bool      _ElementaryCommand::HandleGetString (_ExecutionList& currentProgram){
                 
                 case HY_BL_MODEL: {
                     if (sID>=0) {
-                        _Variable*      theMx = (_Variable*)theObject;
-
-                        if (sID2 < 0) { // get the sID's parameter name
+                        // check to make see if the 
+                       if (sID2 < 0) { // get the sID's parameter name
                             _SimpleList     modelP;
                             _AVLList        modelPA (&modelP);
-                            theMx->ScanForVariables(modelPA,false);
+                            ScanModelForVariables (index, modelPA, false, -1, false);
                             modelPA.ReorderList();
                             if (sID<modelP.lLength) {
                                 result = (_String*)LocateVar(modelP.lData[sID])->GetName()->makeDynamic();
                             } 
 
                         } else { // get the formula for cell (sID, sID2)
-                            _Formula * cellFla = ((_Matrix*)theMx->GetValue())->GetFormula (sID,sID2);
-                            if (cellFla) {
-                                result = new _String((_String*)cellFla->toStr());
+                            if (!IsModelOfExplicitForm (index)) {
+                                _Variable*      theMx = (_Variable*)theObject;
+                                _Formula * cellFla = ((_Matrix*)theMx->GetValue())->GetFormula (sID,sID2);
+                                if (cellFla) {
+                                    result = new _String((_String*)cellFla->toStr());
+                                }
                             }
                         }
                         

src/core/include/batchlan.h
@@ -629,6 +629,8 @@ CheckAssociativeListArg      (_String*);
 void    RetrieveModelComponents      (long, _Matrix*&,     _Matrix*&, bool &);
 void    RetrieveModelComponents      (long, _Variable*&, _Variable*&, bool &);
 bool    IsModelReversible            (long);
+bool    IsModelOfExplicitForm        (long);
+
 void    ReadModelList                (void);
 _String ProcessStringArgument        (_String* data);
 _String*_HBLObjectNameByType         (const long type, const long index, bool correct_for_empties = true);

tests/hbltests/UnitTests/HBLCommands/GetString.bf
@@ -124,15 +124,24 @@ function runTest ()
     binaryModel = {{*,rate1}{R*rate2,*}};
     freqs       = {{0.5,0.5}};
     Model         testModel1 = (binaryModel, freqs, 0);
+    Model         testModel2 =("Exp(binaryModel)",freqs,EXPLICIT_FORM_MATRIX_EXPONENTIAL);
 
-    GetString (modelInfo1, testModel1, 1);
-    GetString (modelInfo2, testModel1, 2);
-    GetString (modelBL, testModel1, -1);
-    GetString (modelBits, testModel1, -2);
-    GetString (modelInfo1_0, testModel1, 1,0);
+    GetString (modelInfo1, testModel1,       1);
+    GetString (modelInfo2, testModel1,       2);
+    GetString (modelBL, testModel1,         -1);
+    GetString (modelBits, testModel1,       -2);
+    GetString (modelInfo1_0, testModel1,    1,0);
+
  
 	assert (Type (modelInfo2) == "Unknown" && modelInfo1 == "rate2" && modelBL == "0.5*rate1+0.5*R*rate2" && modelInfo1_0 == "R*rate2" &&
 	        Type (modelBits) == "AssociativeList" && modelBits["EQ_FREQS"] == "freqs", "Retrieve information about a substitution model");
+
+    GetString (modelInfo1, testModel2,       0);
+    GetString (modelBL, testModel2,         -1);
+    GetString (modelBits, testModel2,       -2);
+    GetString (modelInfo1_0, testModel2,    1,0);
+
+	assert ( modelInfo1 == "rate1" && Type (modelInfo2) == "Unknown" && Type (modelBL) == "Unknown" && Type (modelInfo1_0) == "Unknown" , "Retrieve information about a substitution model in explicit form");
 
 	//-----------------------------------------------------------------------------------------------------------------
 	// Likelihood Functions

tests/hbltests/UnitTests/HBLCommands/fprintf.bf
@@ -1,64 +1,0 @@
-ExecuteAFile (PATH_TO_CURRENT_BF + "TestTools.ibf");
-runATest ();
-
-
-function getTestName () {
-	return "fprintf";
-}		
-
-function getTestedFunctions ()
-{
-	return {{"_ElementaryCommand::HandleFprintf"}};
-}	
-
-
-function runTest () {
-	ASSERTION_BEHAVIOR = 1; /* print warning to console and go to the end of the execution list */
-	testResult  	   = 0;
-
-    fprintf (stdout, "\nTesting basic fprintf functionality. 2+2 = ", 2+2, "\n");
-    
-    _test_file_name = "tmp/fprintf.txt";
-    
-    
-    fprintf (_test_file_name, CLEAR_FILE, KEEP_OPEN, "A string", {{1,2}}, {"a":"bcd"});
-    fprintf (_test_file_name, CLEAR_FILE, KEEP_OPEN, "A string", {{1,2}}, {"a":"bcd"});
-    fprintf (_test_file_name, CLOSE_FILE);
-    fscanf (_test_file_name, "String,NMatrix,Raw", _s, _m, _a);
-    assert (_s == "A string", "fprintf-fscanf roundtrip for failed for a string");
-    assert (Abs (m - {{1,2}}) < 1e-12, "fprintf-fscanf roundtrip for failed for a matrix");
-    fprintf (_test_file_name, CLEAR_FILE,PRINT_SELF, LIST_ALL_VARIABLES);
-
-    _m = {{1,2}};
-    _s = "_m";
-    fprintf (TEMP_FILE_NAME, "This will not live for very long", _s__, "\n");
-    
-    GLOBAL_FPRINTF_REDIRECT = "/dev/null";
-    fprintf (stdout, "Should not be displayed");
-
-    GLOBAL_FPRINTF_REDIRECT = "tmp/redirect.txt";
-    fprintf (stdout, CLEAR_FILE, "Gone to tmp/redirect.txt");
-    GLOBAL_FPRINTF_REDIRECT = "";
-    
-    assert (runCommandWithSoftErrors ("fprintf (\"tmp/tmp/tmp/can't open this\", \"No go\");", "Could not create/open output file at path"), "Failed error checking for an invalid path specification");
-    assert (runCommandWithSoftErrors ("fprintf (stdout, a=b);", "Argument 1 is not a simple expression"), "Failed error checking for trying to print an assignment");
-    assert (runCommandWithSoftErrors ("fprintf (stdout, /a);", "Bad binary operator placement "), "Failed error checking for trying to print a borked expression");
-    
-    
-    /*
-    GetURL (url_data, "http://www.hyphy.org");
-    assert (Abs(url_data) > 0, "Expected to retrieve non-empty data from http://www.hyphy.org");
-    
-    GetURL (PATH_TO_CURRENT_BF + "tmp" + DIRECTORY_SEPARATOR + "GetURL.txt", "http://www.google.com", SAVE_TO_FILE);
-    assert (Abs(url_data) > 0, "Expected to retrieve non-empty data from http://www.hyphy.org");
-    
-    assert (runCommandWithSoftErrors ("GetURL (url_data/j, \"http://www.google.com\")", " is not a valid variable identifier"), "Failed error checking for an invalid receptacle");
-    assert (runCommandWithSoftErrors ("GetURL (url_data, \"http://www.thereisnowaythisurlexists.org\")", "Could not fetch "), "Failed error checking for a URL fetch error");
-    assert (runCommandWithSoftErrors ("GetURL (PATH_TO_CURRENT_BF + \"tmp\" + DIRECTORY_SEPARATOR + \"GetURL.txt\", \"http://www.thereisnowaythisurlexists.org\", SAVE_TO_FILE)", "Could not fetch "), "Failed error checking for a URL fetch error, writing to file");
-    assert (runCommandWithSoftErrors ("GetURL (url_data, \"http://www.hyphy.org\", FAILED_FLAG)", "Unknown action flag "), "Failed error checking for an invalid action flag");
-    */
-    
-	testResult = 1;
-		
-	return testResult;
-}

commit 6344525a634c19f6547d33a69a8b11eb7e6c56a2

Fixes optimizer bug introduced in commit 35db76dd51f35058b53f15956af3bfd2538d32dd

src/core/batchlan2.cpp
@@ -2218,7 +2218,9 @@ void    ScanModelForVariables        (long modelID, _AVLList& theReceptacle, boo
             ((_Matrix*) (LocateVar(modelMatrixIndices.lData[modelID])->GetValue()))->ScanForVariables2(theReceptacle,inclG,modelID2,inclCat);
         } else {
         // formula based
-            ((_Formula*)modelMatrixIndices.lData[modelID])->ScanFForVariables(theReceptacle, false, false, inclCat);
+            // inclG was replaced with false in a previous commit. This caused problems in the optimizer and in
+            // likelihood reporting (it was consistently worse than optimizer results)
+            ((_Formula*)modelMatrixIndices.lData[modelID])->ScanFForVariables(theReceptacle, inclG, false, inclCat);
         }
     }
 }

src/core/formula.cpp
@@ -1659,6 +1659,8 @@ void _Formula::ScanFForVariables (_AVLList&l, bool includeGlobals, bool includeA
         _Operation* theObj = ((_Operation**)theFormula.lData)[i];
         if (theObj->IsAVariable()) {
             if (!includeGlobals)
+                // This change was part of a commit that introduced an optimizer bug (suspected location:
+                // src/core/batchlan2.cpp:2220). This change is suspicious as well (removed and undocumented condition).
                 //if ((((_Variable*)LocateVar(theObj->GetAVariable()))->IsGlobal())||
                  //       (((_Variable*)LocateVar(theObj->GetAVariable()))->ObjectClass()!=NUMBER)) {
                 if (((_Variable*)LocateVar(theObj->GetAVariable()))->IsGlobal()) {