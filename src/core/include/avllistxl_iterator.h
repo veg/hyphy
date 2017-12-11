cfc2356d83ca66e8bd2faa114c3553fdd1919f1f
Josh Soref <jsoref@users.noreply.github.com>
2017-12-11 05:30:37 +0000

    spelling: pointing

--- src/core/include/avllistxl_iterator.h
+++ src/core/include/avllistxl_iterator.h
@@ -87,21 +87,21 @@ public:
   
   /**
    * Return an iterator pointing to the first element of the list
-   * @return the iterator poiting the to the first element fo the list
+   * @return the iterator pointing the to the first element fo the list
   */
   
   AVLListXLIterator& end   (void);
 
   /**
    * Return an iterator pointing past the end of the list
-   * @return the iterator poiting past the end of the list
+   * @return the iterator pointing past the end of the list
    */
 
   AVLListXLIterator& operator ++ (void);
 
   /**
    * Return an iterator pointing past the end of the list
-   * @return the iterator poiting past the end of the list
+   * @return the iterator pointing past the end of the list
   */
 
   
