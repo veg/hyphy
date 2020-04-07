6fd8fab453e301844f2cd06fe8c9802b60c373b2
github-action-benchmark <github@users.noreply.github.com>
2020-04-07 18:31:16 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 6a6cfdcd75d372daf4ec55f62ff2bde93b3285ba

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1586283695350,
+  "lastUpdate": 1586284276960,
   "repoUrl": "https://github.com/veg/hyphy",
   "entries": {
     "Benchmark.js Benchmark": [
@@ -38,6 +38,79 @@ window.BENCHMARK_DATA = {
             "range": "±1.66%",
             "unit": "ops/sec",
             "extra": "76 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "kevin@klevitz.com",
+            "name": "Kevin Levitz",
+            "username": "kjlevitz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6a6cfdcd75d372daf4ec55f62ff2bde93b3285ba",
+          "message": "Update bench.js",
+          "timestamp": "2020-04-07T14:28:45-04:00",
+          "tree_id": "5e5fb82e91823cb63d975796167d4c3ec14c78a6",
+          "url": "https://github.com/veg/hyphy/commit/6a6cfdcd75d372daf4ec55f62ff2bde93b3285ba"
+        },
+        "date": 1586284269949,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "SmallCodon.bf",
+            "value": 439,
+            "range": "±3.32%",
+            "unit": "ops/sec",
+            "extra": "68 samples"
+          },
+          {
+            "name": "IntermediateProtein.bf",
+            "value": 393,
+            "range": "±1.49%",
+            "unit": "ops/sec",
+            "extra": "76 samples"
+          },
+          {
+            "name": "FEL.wbf",
+            "value": 380,
+            "range": "±1.43%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "SLAC.wbf",
+            "value": 380,
+            "range": "±1.30%",
+            "unit": "ops/sec",
+            "extra": "73 samples"
+          },
+          {
+            "name": "SLAC-partitioned.wbf",
+            "value": 380,
+            "range": "±1.44%",
+            "unit": "ops/sec",
+            "extra": "80 samples"
+          },
+          {
+            "name": "MEME.wbf",
+            "value": 367,
+            "range": "±3.28%",
+            "unit": "ops/sec",
+            "extra": "79 samples"
+          },
+          {
+            "name": "BUSTED.wbf",
+            "value": 374,
+            "range": "±1.34%",
+            "unit": "ops/sec",
+            "extra": "74 samples"
           }
         ]
       }
