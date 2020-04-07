3dc8a1d6701b0315215ee89577307d79b50ecc94
github-action-benchmark <github@users.noreply.github.com>
2020-04-07 18:21:35 +0000

    add Benchmark.js Benchmark (benchmarkjs) benchmark result for 37e8916da7e148093e358b0cbb13d2c7fb48990e

---
+++ dev/bench/data.js
@@ -0,0 +1,46 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1586283695350,
+  "repoUrl": "https://github.com/veg/hyphy",
+  "entries": {
+    "Benchmark.js Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "kevin@klevitz.com",
+            "name": "Kevin Levitz",
+            "username": "kjlevitz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "37e8916da7e148093e358b0cbb13d2c7fb48990e",
+          "message": "Update benchmark.yml",
+          "timestamp": "2020-04-07T14:19:26-04:00",
+          "tree_id": "ffac3f6b4eee2fae9d0410858b5a64916c299b1c",
+          "url": "https://github.com/veg/hyphy/commit/37e8916da7e148093e358b0cbb13d2c7fb48990e"
+        },
+        "date": 1586283688463,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "SmallCodon.bf",
+            "value": 519,
+            "range": "±3.18%",
+            "unit": "ops/sec",
+            "extra": "66 samples"
+          },
+          {
+            "name": "IntermediateProtein.bf",
+            "value": 441,
+            "range": "±1.66%",
+            "unit": "ops/sec",
+            "extra": "76 samples"
+          }
+        ]
+      }
+    ]
+  }
+}
