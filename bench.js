403f9b6d8f610c072e1d0b408b8f621ba7a77332
Kevin Levitz <kevin@klevitz.com>
2020-04-27 18:23:42 -0400

    fix: Resolved async issues with Benchmarking
    This commit is to address previous concerns with OPS.
    OPS was being calculated by simply starting the test- it did not wait for it to finish before reporting it's operations.
    **Notice: This will absolutely cause the current benchmarks to trigger their performance alerts. You can safely ignore them**

--- bench.js
+++ bench.js
@@ -1,90 +1,90 @@
 var Benchmark = require('benchmark');
-const { exec } = require("child_process");
 var suite = new Benchmark.Suite;
-
+var execSync = require('child_process').execSync;
 
 // add tests
-suite.add('SmallCodon.bf', function() {
-  exec("./HYPHYMP 'tests/hbltests/SimpleOptimizations/SmallCodon.bf'", (error, stdout, stderr) => {
-      if (error) {return;}
+suite
+.add('SmallCodon.bf', function() {
+  execSync("./HYPHYMP 'tests/hbltests/SimpleOptimizations/SmallCodon.bf'", (error, stdout, stderr) => {
+      if (error) { console.log("Error with job = " + error); return;}
       if (stderr) {return;}
   });
 })
 .add('IntermediateProtein.bf', function() {
-  exec("./HYPHYMP 'tests/hbltests/SimpleOptimizations/IntermediateProtein.bf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/SimpleOptimizations/IntermediateProtein.bf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('FEL.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/FEL.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/FEL.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('SLAC.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/SLAC.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/SLAC.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('SLAC-partitioned.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/SLAC-partitioned.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/SLAC-partitioned.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('MEME.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/MEME.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/MEME.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('BUSTED.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/BUSTED.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/BUSTED.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('BUSTED-SRV.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/BUSTED-SRV.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/BUSTED-SRV.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('RELAX.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/RELAX.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/RELAX.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('FUBAR.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/FUBAR.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/FUBAR.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('BGM.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/BGM.wbf''", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/BGM.wbf''", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('CFEL.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/CFEL.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/CFEL.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('FADE.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/FADE.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/FADE.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('GARD.wbf', function() {
-  exec("./HYPHYMP 'tests/hbltests/libv3/GARD.wbf'", (error, stdout, stderr) => {
-    if (error) {return;}
+  execSync("./HYPHYMP 'tests/hbltests/libv3/GARD.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
