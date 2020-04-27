2accd3e0e5a04d01662877dc430e87b03593af58
Kevin Levitz <kevin@klevitz.com>
2020-04-27 19:51:32 -0400

    Update bench.js

--- bench.js
+++ bench.js
@@ -17,73 +17,73 @@ suite
   });
 })
 .add('FEL.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/FEL.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/FEL.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('SLAC.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/SLAC.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/SLAC.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('SLAC-partitioned.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/SLAC-partitioned.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/SLAC-partitioned.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('MEME.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/MEME.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/MEME.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('BUSTED.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/BUSTED.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/BUSTED.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('BUSTED-SRV.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/BUSTED-SRV.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/BUSTED-SRV.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('RELAX.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/RELAX.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/RELAX.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('FUBAR.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/FUBAR.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/FUBAR.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('BGM.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/BGM.wbf''", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/BGM.wbf''", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('CFEL.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/CFEL.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/CFEL.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('FADE.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/FADE.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/FADE.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 .add('GARD.wbf', function() {
-  execSync("./HYPHYMP 'tests/hbltests/libv3/GARD.wbf'", (error, stdout, stderr) => {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/GARD.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
