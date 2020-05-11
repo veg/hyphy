a169756f98d6fabfae3849a0372d1c568500fea9
Kevin Levitz <kevin@klevitz.com>
2020-05-11 17:21:45 -0400

    add: Replaced temp removed code for full tests

--- bench.js
+++ bench.js
@@ -10,8 +10,94 @@ suite
       if (stderr) {return;}
   });
 })
+.add('IntermediateProtein.bf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/SimpleOptimizations/IntermediateProtein.bf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('FEL.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/FEL.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('SLAC.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/SLAC.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('SLAC-partitioned.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/SLAC-partitioned.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('MEME.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/MEME.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('BUSTED.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/BUSTED.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('BUSTED-SRV.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/BUSTED-SRV.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('RELAX.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/RELAX.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('FUBAR.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/FUBAR.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('BGM.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/BGM.wbf''", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('CFEL.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/CFEL.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+.add('FADE.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/FADE.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
 .add('GARD.wbf', function() {
   execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/GARD.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+
+.add('aBSREL.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/aBSREL.wbf'", (error, stdout, stderr) => {
+    if (error) { console.log("Error with job = " + error); return;}
+    if (stderr) {return;}
+  });
+})
+
+.add('aBSREL-MH.wbf', function() {
+  execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/aBSREL-MH.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
