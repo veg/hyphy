7efca802439049b29c66909e7afa03ca2f5e2690
Kevin Levitz <kevin@klevitz.com>
2020-08-27 16:27:46 -0400

    fix: Casing for ABSREL tests

--- bench.js
+++ bench.js
@@ -96,14 +96,14 @@ suite
   });
 })
 
-.add('aBSREL.wbf', function() {
+.add('ABSREL.wbf', function() {
   execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/aBSREL.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
   });
 })
 
-.add('aBSREL-MH.wbf', function() {
+.add('ABSREL-MH.wbf', function() {
   execSync("/home/runner/work/hyphy/hyphy/HYPHYMP 'tests/hbltests/libv3/aBSREL-MH.wbf'", (error, stdout, stderr) => {
     if (error) { console.log("Error with job = " + error); return;}
     if (stderr) {return;}
